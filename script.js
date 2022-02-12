b018924307a18f8deb425d70ca05555af165ebe8
sleptsovstas <sleptsovstas.v@gmail.com>
2022-02-13 00:43:33 +0900

    guess the number

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// console.log(document.querySelector('.message').textContent)
+// document.querySelector('.message').textContent = '🎉 Correct Number!'
+
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent = 10
+
+// document.querySelector('.guess').value = 23
+// console.log(document.querySelector('.guess').value)
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = +document.querySelector('.guess').value
+  console.log(guess)
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!'
+  }
+})
